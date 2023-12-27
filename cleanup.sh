c9cb1906bfd212c68977a3e041b711c4f3994a1d
Valentina Mazzotti <37270513+valimzztt@users.noreply.github.com>
2023-12-27 17:11:18 +0100

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -2,7 +2,7 @@
 
 cd 03_WANNIER_HSE
 
-#clean up everything generate by ASE
+#clean up everything generate by VASP and Wannier90
 rm -f CHG CONTCAR* DOSCAR* DYNMAT EIGENVAL IBZKPT OPTIC OSZICAR* OUTCAR* PROCAR* \
       PCDAT XDATCAR* PARCHG* vasprun.xml SUMMARY.* REPORT \
       wannier90_band.gnu wannier90_band.kpt wannier90.chk wannier90.err wannier90.wout wannier90_band.kpt wannier90_hr.dat wannier90_centres.xyz wannier90.nnkp wannier90.mmn wannier90.eig\
